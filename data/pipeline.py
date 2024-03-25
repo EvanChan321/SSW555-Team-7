c473326921db92c3501e04fcfd2cb59c0324cc00
Andrew Hing <ahing1@stevens.edu>
2024-03-25 16:41:41 -0400

    make retriving pipeline data function

--- data/pipeline.py
+++ data/pipeline.py
@@ -1,2 +1,12 @@
 # Pipeline For Data
 
+def retrieve_pipeline_data(pipeline):
+    """
+    Retrieve data from a given pipeline.
+    Returns:
+    The data retrieved from the pipeline.
+    """
+    data = []
+    for item in pipeline:
+        data.append(item)
+    return data
