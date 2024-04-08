0eb405f0094e9e2fff72debcbb17f88699ce0074
notalim <alimneedstowork@gmail.com>
2024-04-08 16:33:02 -0400

    I NEED DATA

--- server/server.js
+++ server/server.js
@@ -1,37 +1,24 @@
-// server.mjs
 import express from "express";
-import bodyParser from "body-parser";
-import { spawn } from "child_process";
+import runPythonInference from "./script.js";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
-
+app.use(cors());
 app.use(express.json());
 
-app.post("/submit-eeg", (req, res) => {
-    const eegData = req.body;
+const PORT = 3000;
 
-    const pythonProcess = spawn("python", [
-        "./server/model.py",
-        JSON.stringify(eegData),
-    ]);
-
-    pythonProcess.stdout.on("data", (data) => {
-        try {
-            const result = JSON.parse(data);
-            res.json({ probability: result.probability });
-        } catch (error) {
-            console.error("Error parsing JSON from python script", error);
-            res.status(500).send("Error processing the EEG data.");
-        }
-    });
-
-    pythonProcess.stderr.on("data", (data) => {
-        console.error(`stderr: ${data}`);
-        res.status(500).send(
-            "An error occurred while processing the EEG data."
-        );
-    });
+app.post("/analyze-eeg", async (req, res) => {
+    try {
+        const eegData = req.body; 
+        const result = await runPythonInference(eegData);
+        res.json({ inferenceResult: result });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Failed to process EEG data");
+    }
 });
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
